fbd5f6e5c0d21ebea70d74a0c596399c94901607
Mari Coronado <159261501+marianacoronado19@users.noreply.github.com>
2024-07-31 09:21:19 -0300

    Add files via upload

---
+++ funções.js
@@ -0,0 +1,22 @@
+let paris = document.getElementById ("Torre")
+let leg = document.getElementById ("legenda")
+
+function torre(){
+    paris.src = 'Imagens/Torre.jpg'
+    leg.innerHTML = 'Torre Eiffel'
+}
+
+function louvre(){
+    paris.src = 'Imagens/Louvre.jpg'
+    leg.innerHTML = 'Museu do Louvre'
+}
+
+function arco(){
+    paris.src = 'Imagens/Arco do Triunfo.jpg'
+    leg.innerHTML = 'Arco do Triunfo'
+}
+
+function cidade(){
+    paris.src = 'Imagens/Paris.jpg'
+    leg.innerHTML = 'Cidade de Paris'
+}
